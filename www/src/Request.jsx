970513bf5ae4615504abfc84abd88e73f6e572ef
sergix <sergix@outlook.com>
2024-10-19 11:59:55 -0400

    add: info box

--- www/src/Request.jsx
+++ www/src/Request.jsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form"
 import { Form, FormControl, FormLabel, FormDescription, FormItem, FormField } from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 const submitFormSchema = z.object({
     test: z.string()
@@ -25,15 +26,31 @@ export default function Submit() {
 
     return (
         <>
-            <div>
-                <h1>Request Help</h1>
+            <h1>Request Help</h1>
+            <div className='rounded-sm bg-slate-200 p-4 my-2'>
+                {/* TODO: avatar image, name, address */}
+                <div className="my-1">
+                    <div className='flex flex-row align-middle'>
+                        <Avatar>
+                            <AvatarImage src='https://northsidecharleston.com/wp-content/uploads/2023/07/profile.jpg'/>
+                            <AvatarFallback>PM</AvatarFallback>
+                        </Avatar>
+                        <p className="ml-2 leading-9">
+                            Peyton McGinnis
+                        </p>
+                    </div>
+                    <address className="my-2 text-sm">
+                        8609 Woodland Walk<br/>
+                        North Charleston, SC 29420
+                    </address>
+                </div>
             </div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
                     <FormField name='test' control={form.control} render={({field}) => (
                         <FormItem>
-                            <FormLabel></FormLabel>
-                            <FormDescription>This is a form.</FormDescription>
+                            <FormLabel>Location</FormLabel>
+                            <FormDescription>Saved Address</FormDescription>
                             <FormControl>
                                 <Input {...field} />
                             </FormControl>
