41952fbd497288954d894139e40aff9f36e341d6
Aidan Nuzum-Clark <aidan@Aidans-Laptop.local>
2024-10-20 12:39:59 -0400

    Added basic offline page components.

--- www/src/Offline.jsx
+++ www/src/Offline.jsx
@@ -1,15 +1,37 @@
 import NavBar from "./components/Navigation/NavBar"
+import BlueToothMesh from "./components/Offline/BluetoothMesh"
+import EmergencyContact from "./components/Offline/EmergencyContact"
+import WaffleHouse from "./components/Offline/WaffleHouse"
+import { Button } from "@/components/ui/button"
+import BeatLoader from "react-spinners/BeatLoader";
+import { useState } from "react";
+
 
 export default function Offline() {
+    const [loading, setLoading] = useState(false);
+    const [connectionChecked, setConnectionChecked] = useState(null);
+    const checkConnection = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            setConnectionChecked(new Date().toLocaleTimeString());
+        }, 2000);
+    }
     return (
         <>
             <NavBar />
-            <main className='px-4 mt-24 mb-8'>
+            <main class='px-4 mt-24 mb-8'>
                 <h1>You are offline.</h1>
-                <div className="rounded-md p-4 bg-red-700 text-white mt-4">
-                    <h2 className="text-sm font-bold mb-2">EMERGENCY LOCATION</h2>
-                    <p>blah</p>
+                <p>Last online 5:42pm</p>
+                {connectionChecked && <p>Connection checked at {connectionChecked}</p>}
+                <div class="flex gap-2 items-center"><Button onClick={()=>{
+                    checkConnection();
+                }}>Test Connection</Button>
+                {loading && <BeatLoader color={"#000"} size={8} />}
                 </div>
+                <EmergencyContact />
+                <WaffleHouse />
+                <BlueToothMesh />
             </main>
         </>
     )
---
+++ www/src/components/Offline/EmergencyContact.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+export default function EmergencyContact() {
+	return (
+		<div class="rounded-md p-4 bg-red-600 text-white mt-4">
+			<h2 class="text-sm font-bold mb-2">EMERGENCY Contact</h2>
+            <p>Joseph Joestar</p>
+			<div class="flex justify-between">
+            </div>
+		</div>
+	);
+}
