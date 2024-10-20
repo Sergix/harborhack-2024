8210d4983334a543fad029ee7882e72352fcee3f
sergix <sergix@outlook.com>
2024-10-20 14:09:31 -0400

    add: subscription dialog

--- www/src/Offline.jsx
+++ www/src/Offline.jsx
@@ -1,4 +1,3 @@
-import NavBar from "./components/Navigation/NavBar"
 import BlueToothMesh from "./components/Offline/BluetoothMesh"
 import EmergencyContact from "./components/Offline/EmergencyContact"
 import WaffleHouse from "./components/Offline/WaffleHouse"
@@ -6,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import BeatLoader from "react-spinners/BeatLoader";
 import { useState } from "react";
 import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
 
 
 export default function Offline() {
@@ -20,11 +20,10 @@ export default function Offline() {
     }
     return (
         <>
-            {/* <NavBar /> */}
-        <main className='px-4 mb-8 pt-4 bg-neutral-900 text-white h-screen'>
-                <img className='bg-neutral-500 acrylic fixed w-32 px-4 py-2 rounded-md' src='/logo.png'></img>
+        <main className='px-4 mb-8 pt-4 bg-neutral-900 text-white h-screen phone-size'>
+                <img className='bg-neutral-800 fixed w-32 px-4 py-2 rounded-md' src='/logo-white.png'></img>
                 <div className='rounded-sm pt-2 mt-24'>
-                    <h1 className='text-3xl'>You are offline.</h1>
+                    <h1 className='text-4xl'>You are offline.</h1>
                     <p className='my-2 text-xs font-bold'>INFORMATION BELOW ACCURATE AS OF <Badge className='ml-1 bg-sky-900'>5:42 PM</Badge></p>
                 </div>
                 <div className="flex gap-2 items-center">
@@ -34,6 +33,7 @@ export default function Offline() {
                 {!loading && connectionChecked && <p className='text-xs font-bold'>CONNECTION LAST CHECKED AT {connectionChecked}</p>}
                 {loading && <BeatLoader color={"#fff"} size={8} />}
                 </div>
+                <Separator className='mt-4'/>
                 <EmergencyContact />
                 <BlueToothMesh />
                 <WaffleHouse />
--- www/src/components/Navigation/NavBar.jsx
+++ www/src/components/Navigation/NavBar.jsx
@@ -26,7 +26,7 @@ export default function NavBar({ page }) {
 			<div className='m-2 shadow-xl px-3 py-2 rounded-full bg-neutral-200 justify-center content-center acrylic'>
 			<NavigationMenu className='mx-auto'>
 				<NavigationMenuList>
-					<img className="w-24 mr-auto pr-4" src='/logo.png'></img>
+					<a href='/'><img className="w-24 mr-auto pr-4" src='/logo.png'></img></a>
 					<NavigationMenuItem onClick={()=>navigate("/")}>
 						<NavigationMenuLink className={'!bg-neutral-200 hover:!bg-neutral-300 acrylic ' + navigationMenuTriggerStyle() + getPageStyle('home') }>
 							Browse
